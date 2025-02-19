a4c263f18d46333b36e558d3d23839f7b8c779a1
Digital GaB <digitallgab@gmail.com>
2025-02-19 01:28:04 -0300

    feat: implementando http types

---
+++ src/http_types/http_request.py
@@ -0,0 +1,4 @@
+class HttpRequest:
+    def __init__(self, body, param) -> None:
+        self.body = body
+        self.param = param
--- src/main/routes/event.py
+++ src/main/routes/event.py
@@ -1,12 +1,16 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from src.http_types.http_respone import HttpResponse
+from src.http_types.http_request import HttpRequest
 
 event_route = Blueprint("event_route", __name__)
 
 @event_route.route("/events", methods=["GET"])
 def list():
-    return jsonify({"message": "Events returned"}), 200
+    http_response = HttpResponse(200, {}, {"message": "List Events"})
+    return http_response    
+
 @event_route.route("/event", methods=["POST"])
 def create():
-    return jsonify({"message": "Event created"}), 201
-
+    http_request = HttpRequest(body=request.json)
+    return http_request.body
     
