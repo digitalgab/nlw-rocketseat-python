679789b8e4bf92f31f1a71a7015c4e241dd468ef
Digital GaB <digitallgab@gmail.com>
2025-02-21 13:13:16 -0300

    feat: ajustando server.js

--- src/main/server/server.py
+++ src/main/server/server.py
@@ -1,5 +1,9 @@
 from flask import Flask
 from src.main.routes.event import event_route
+from src.main.routes.subscribers import subscribers_route
+from src.main.routes.events_link import event_link_route
 
 app = Flask(__name__)
 app.register_blueprint(event_route)
+app.register_blueprint(subscribers_route)
+app.register_blueprint(event_link_route)    
